92532e507c27a3bc93f2e911930ff25cf7cd2adf
Jorky M <yorki09maga@gmail.com>
2023-02-09 16:30:11 -0600

    index del proyecto

--- Intro to Lit/my-elements.ts
+++ Intro to Lit/my-elements.ts
@@ -1,35 +1,61 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement, state, query} from 'lit/decorators.js';
+
+type ToDoItem = {
+  text: string,
+  completed: boolean
+}
 
 @customElement('todo-list')
 export class ToDoList extends LitElement {
+
+  // TODO: Add styles here
+  static styles = css`
+  .completed {
+    text-decoration-line: line-through;
+    color: #777;
+  }
+`;
+
   @state()
   private _listItems = [
-    { text: 'Start Lit tutorial', completed: true },
-    { text: 'Make to-do list', completed: false }
+    { text: 'Make to-do list', completed: true },
+    { text: 'Add some styles', completed: true }
   ];
 
   render() {
     return html`
       <h2>To Do</h2>
       <ul>
-         <!-- TODO: Render list items. -->
-         ${this._listItems.map((item) =>
-           html`<li>${item.text}</li>`
-             )}
+        ${this._listItems.map((item) =>
+          html`
+            <li
+                class="TODO"
+                class=${item.completed ? 'completed' : ''}
+                @click=${() => this.toggleCompleted(item)}>
+          
+              ${item.text}
+          
+          
+            </li>`
+        )}
       </ul>
       <input id="newitem" aria-label="New item">
       <button @click=${this.addToDo}>Add</button>
     `;
   }
 
-  // TODO: Add click handler.
+  toggleCompleted(item: ToDoItem) {
+    item.completed = !item.completed;
+    this.requestUpdate();
+  }
+
   @query('#newitem')
   input!: HTMLInputElement;
-  
+
   addToDo() {
     this._listItems = [...this._listItems,
-      {text: this.input.value, completed: false}];
+        {text: this.input.value, completed: false}];
     this.input.value = '';
   }
 }
