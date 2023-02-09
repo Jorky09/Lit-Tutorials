4231ebd1b15d3acdcb5d5b57531cc02a76468183
Jorky M <yorki09maga@gmail.com>
2023-02-09 17:26:27 -0600

    tutorial 2

--- Working whit lists/my elements.ts
+++ Working whit lists/my elements.ts
@@ -1,40 +1,31 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {map} from 'lit/directives/map.js'
-// TODO: import repeat directive.
-import {repeat} from 'lit/directives/repeat.js';
-    
+import {map} from 'lit/directives/map.js';
+
 @customElement('my-element')
 class MyElement extends LitElement {
+  private _deleteThing(index: number) {
+    this.things = this.things.filter((_, i) => i !== index);
+  }
   @state()
-  tasks = [
-    { id: 'a', label: 'Learn Lit'},
-    { id: 'b', label: 'Feed the cat'},
-    { id: 'c', label: 'Go for a walk'},
-    { id: 'd', label: 'Take a nap'},
-  ]
+  things = [
+    "Raindrops on roses",
+    "Whiskers on kittens",
+    "Bright copper kettles",
+    "Warm woolen mittens",
+  ];
 
   render() {
     return html`
-      <p>Things to do today:</p>
-      <button @click=${() => this._sort(1)}>Sort ascending</button>
-      <button @click=${() => this._sort(-1)}>Sort descending</button>
+      <p>A few of my favorite things</p>
       <ul>
-        <!-- TODO: Replace below using repeat directive. -->
-        ${repeat(
-          this.tasks,
-          (task) => task.id,
-          (task) => html`
-            <li>
-              <label><input type="checkbox" />${task.label} (${task.id})</label>
-            </li>
-          `
-        )}
+        <!-- TODO: Add click event handlers for the delete button  below. -->
         ${map(
-          this.tasks,
-          (task) => html`
+          this.things,
+          (thing, index) => html`
             <li>
-              <label><input type="checkbox" />${task.id}) ${task.label}</label>
+              ${thing}
+              <button @click=${() => this._deleteThing(index)}>Delete</button>
             </li>
           `
         )}
@@ -42,8 +33,5 @@ class MyElement extends LitElement {
     `;
   }
 
-  private _sort(dir: number) {
-    this.tasks.sort((a, b) => a.label.localeCompare(b.label) * dir);
-    this.requestUpdate();
-  }
+  // TODO: Implement method to delete an item.
 }
