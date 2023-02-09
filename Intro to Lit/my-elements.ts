1bb6b3f3850f99e8eb24ddef0aed6d3820124b9d
Jorky M <yorki09maga@gmail.com>
2023-02-09 16:14:04 -0600

    index del proyecto

--- Intro to Lit/my-elements.ts
+++ Intro to Lit/my-elements.ts
@@ -1,15 +1,21 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
-// TODO: Add a reactive property
+@customElement('name-tag')
+export class NameTag extends LitElement {
   @property()
-  message: string = 'Hola de nuevo.';
+  name: string = 'Your name here';
 
   render() {
+    // TODO: Add declarative event listener to input.
     return html`
-      <p>TODO: ${this.message}</p>
+      <p>Hello, ${this.name}</p>
+      <input @input=${this.changeName} placeholder="Enter your name">
     `;
   }
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.name = input.value;
+}
+  // TODO: Add event handler method.
 }
