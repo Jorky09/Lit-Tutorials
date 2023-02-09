df6f1771cec498f49c7b24d398301ecf134c5fd7
Jorky M <yorki09maga@gmail.com>
2023-02-09 16:08:09 -0600

    index del proyecto

--- Intro to Lit/my-elements.ts
+++ Intro to Lit/my-elements.ts
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <script type="module" src="my-element.js"></script>
-    <style>
-      body {
-        font-family: 'Open Sans', sans-serif;
-        font-size: 1.5em;
-        padding-left: 0.5em;
-      }
-    </style>
-  </head>
-  <body>
-    <my-element></my-element>
-  </body>
-</html>
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  render() {
+    return html`
+      <p>Hola mundo! De Mi llave.</p>
+    `;
+  }
+}
